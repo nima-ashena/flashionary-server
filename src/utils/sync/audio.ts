18cbe3a8d5e96365e7b9a827c457f9a2b010de23
nima <nima.ashena76@gmail.com>
2024-12-09 20:31:10 +0330

    FIX: sync audio

--- src/utils/sync/audio.ts
+++ src/utils/sync/audio.ts
@@ -9,47 +9,38 @@ import { Sentence } from "../../models/sentence.model";
 export const syncAllAudios = async (): Promise<any> => {
    try {
       return new Promise(async (resolve, reject) => {
-
          const vocabs = await Vocab.find();
          for (let i in vocabs) {
             const audio = vocabs[i].audio
-
             const path = Path.resolve(
                __dirname,
+               '..',
                '..',
                '..',
                'static',
                'audios',
                audio,
             );
-
             fs.stat(path, async (err, stats) => {
                // lack of file
-               if (err || stats.size < 1000) {
-                  await textToAudioOneApi(vocabs[i].title, vocabs[i].audio);
+               if (err) {
+                   await textToAudioOneApi(vocabs[i].title, vocabs[i].audio);
                }
-
-               // stats.isFile(); // true
-               // stats.isDirectory(); // false
-               // stats.isSymbolicLink(); // false
-               // stats.size; // 1024000 //= 1MB
-            });
-
+           });
          }
 
          const sentences = await Sentence.find();
          for (let i in sentences) {
             const audio = sentences[i].audio
-
             const path = Path.resolve(
                __dirname,
+               '..',
                '..',
                '..',
                'static',
                'audios',
                audio,
             );
-
             fs.stat(path, async (err, stats) => {
                // lack of file
                if (err || stats.size < 1000) {
